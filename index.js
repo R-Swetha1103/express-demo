db62077788e2ae084ef11f43722ec91fa333d5ee
R-Swetha1103 <swetharagupathy1103@gmail.com>
2024-09-13 14:52:30 +0530

    fs

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-// import the express module
-const express = require('express');
-
-// create an express application
+const express = require ('express');
+//create an express application
 const app = express();
-
-//define the routes and their corresponding function
-app.get('/', (req, res) => {
-  res.send("GET World");
+const fs = require ('fs');
+//defines the routes and their correponding functions
+app.get('/stats', (req, res) => {
+  fs.stat('./files/test.txt', (err, stats) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({
+        size: '${stats.size} bytes',
+        isFile: stats.isFile(),
+        isDirectory: stats.isDirectory(),
+        isSymbolicLink: stats.isSymbolicLink(),
+      });
+    }
+  })
+});
+app.post('/create', (req,res) =>{
+  fs.writeFile('./files/newFile.txt', 'Hello World', (err) =>{
+    if (err) {
+      res.send(err);
+    }
+    res.send('File created successfully');
+  })
 });
 
-app.post('/', (req, res) => {
-  res.send("POST World");
-});
+app.get('/read', (req,res) =>{
+  fs.readFile('./files/test.txt', 'utf8', (err, data) =>{
+    if (err) {
+      res.send(err);
+    }
+    res.send('data');
+  })
+});     
 
-app.get('/test', (req, res) => {
-  res.send("GET Test");
-});
-
-//start the server by listining on a part for incoming requests
-app.listen(3001, "localhost", () => {
+app.get('/copy', (req, res) => {
+  fs.copyFile('./files/fileA.txt', './files/fileB.txt', (err) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send('File copied successfully')
+  })
+}); 
+app.listen(3001, "localhost", () =>{
   console.log("Server is running on http://localhost:3001");
-});
+})
